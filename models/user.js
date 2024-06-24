c66f48d756229f43a4a21ec5cb3195c4a86f2137
Sahil-Vaidya <sahilvaidyasv2003@gmail.com>
2024-06-24 20:35:07 +0530

    Initial commit on main branch

---
+++ models/user.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// Define the Person schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String
+    },
+    mobile: {
+        type: String
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    aadharCardNumber: {
+        type: Number,
+        required: true,
+        unqiue: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['voter', 'admin'],
+        default: 'voter'
+    },
+    isVoted: {
+        type: Boolean,
+        default: false
+    }
+});
+
+// before save the password check all
+userSchema.pre('save', async function(next){
+    const person = this;
+
+    // Hash the password only if it has been modified or is new
+    if(!person.isModified('password')) return next();
+    try{
+        // hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+        
+        // Override the plain password with the hashed one
+        person.password = hashedPassword;
+        next();
+    }catch(err){
+        return next(err);
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
